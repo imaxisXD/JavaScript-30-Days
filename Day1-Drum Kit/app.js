06b5b44c6dfd00e2d7c7e73bcd06e241be7b9c9d
Abhishek <abhishek@publicissapient.com>
2021-06-20 01:15:08 +0530

    day1 completed

---
+++ Day1-Drum Kit/app.js
@@ -0,0 +1,23 @@
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    if (!audio)
+        return;
+    const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+
+    key.classList.add('playing');
+
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function removeTransition(e) {
+
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+
+}
